b9a5cfe51df6447018a36bd6e2e134bbb9b9aa7b
Amir h. Ahmadi <amir.ahmadi@icesevices.com>
2017-07-25 17:25:07 +0200

    Rollback previous commit

--- src/main/java/com/ice/av/sample/service/StatisticService.java
+++ src/main/java/com/ice/av/sample/service/StatisticService.java
@@ -21,6 +21,6 @@ public class StatisticService {
 
     @RequestMapping(value = "/info", method = RequestMethod.GET)
     public Info getInfo(){
-        return new Info().withInfo("This is a test written by Amir...");
+        return new Info();//.withInfo("This is a test written by Amir...");
     }
 }
